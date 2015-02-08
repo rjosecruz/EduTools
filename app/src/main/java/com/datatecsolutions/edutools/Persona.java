1f7c6501737289677d2dd775298e4f487c6a7860
DAVID <jose.david.mayorga@gmail.com>
2015-02-07 20:49:10 -0600

    Subida de proyecto

---
+++ app/src/main/java/com/datatecsolutions/edutools/Persona.java
@@ -0,0 +1,22 @@
+package com.datatecsolutions.edutools;//Source file: C:\\Users\\DAVID\\Google Drive\\proyecto maestros\\Clases Java\\Persona.java
+
+
+public class Persona {
+    private int primerNombre;
+    private int segundoNombre;
+    private int primerApellido;
+    private int segundoApellido;
+    private int fechaNacimiento;
+    private int edad;
+    private int nacionalidad;
+    private int sexo;
+
+    /**
+     * @roseuid 54C2A658005A
+     */
+    public Persona() {
+
+    }
+   
+
+}
