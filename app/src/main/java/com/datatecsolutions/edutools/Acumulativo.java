c733de82affe93b859f2fceb636c00402b7e4edb
DAVID <jose.david.mayorga@gmail.com>
2015-02-07 19:59:42 -0600

    Subida de proyecto

---
+++ app/src/main/java/com/datatecsolutions/edutools/Acumulativo.java
@@ -0,0 +1,65 @@
+//Source file: C:\\Users\\DAVID\\Google Drive\\proyecto maestros\\Clases Java\\Acumulativo.java
+
+
+/**
+ * @author datatec solutions
+ * @version 1.0
+ */
+public class Acumulativo {
+    private String nombre;
+    private String clase;
+    private String seccion;
+    private String tipo;
+    private Double valor = 0.0;
+    private char parcial = {
+            I,
+            II,
+            II,
+            IV
+    };
+    protected Integer idAcumlativo;
+
+    /**
+     * @roseuid 54C2A5A2017C
+     */
+    public Acumulativo() {
+
+    }
+
+    /**
+     * @roseuid 54C1DBAD038D
+     */
+    public void nuevoAcumulativo() {
+
+    }
+
+    /**
+     * @roseuid 54C1DBF80135
+     */
+    public void guardarAcumulativo() {
+
+    }
+
+    /**
+     * @param idSeccion
+     * @return Acumulativo
+     * @roseuid 54C1DE820231
+     */
+    public Acumulativo obtenerAcumulativo(Integer idSeccion) {
+        return null;
+    }
+
+    /**
+     * @roseuid 54C262560102
+     */
+    public void toString() {
+
+    }
+
+    /**
+     * @roseuid 54C29FCA0025
+     */
+    public void eliminarAcumulativo() {
+
+    }
+}
