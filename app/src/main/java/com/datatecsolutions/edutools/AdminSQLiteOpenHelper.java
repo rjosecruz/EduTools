906f90f170d9054d4b9e1be42ad96290353a10d8
DAVID <jose.david.mayorga@gmail.com>
2015-02-07 22:06:58 -0600

    Subida de proyecto

--- app/src/main/java/com/datatecsolutions/edutools/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/datatecsolutions/edutools/AdminSQLiteOpenHelper.java
@@ -18,13 +18,19 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table alumnos(rne integer primary key, nombre text, apellido text, correo text,telefono text,sexo text,fechaNac text)");
         db.execSQL("create table acumulativos(id_acumulativo integer primary key, id_seccion integer, descripcion text, id_tipo_acumulativo text,fecha text,valor real,id_clase text)");
+        db.execSQL("create table notas_acumulativas(id_acumulativo integer, rne text, nota real)");
+        db.execSQL("create table tipo_acumulativos(id_tipo_acumulativo integer primary key, descripcion text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int versionAnte, int versionNue) {
         db.execSQL("drop table if exists alumnos");
         db.execSQL("drop table if exists acumulativos");
+        db.execSQL("drop table if exists notas_acumulativas");
+        db.execSQL("drop table if exists tipo_acumulativos");
         db.execSQL("create table alumnos(dni integer primary key, nombre text, colegio text, nromesa integer)");
         db.execSQL("create table acumulativos(id_acumulativo integer primary key, id_seccion integer, descripcion text, id_tipo_acumulativo text,fecha text,valor real,id_clase text)");
+        db.execSQL("create table notas_acumulativas(id_acumulativo integer, rne text, nota real)");
+        db.execSQL("create table tipo_acumulativos(id_tipo_acumulativo integer primary key, descripcion text)");
     }
 }
