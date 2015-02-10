bb9f9ddc1e928ac3eac5fa88ba71ee7174ce03a9
DAVID <jose.david.mayorga@gmail.com>
2015-02-10 06:24:58 -0600

    Subida de proyecto

--- app/src/main/java/com/datatecsolutions/edutools/crear_acumulativo.java
+++ app/src/main/java/com/datatecsolutions/edutools/crear_acumulativo.java
@@ -4,31 +4,21 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 
 
-public class activity_crear_acumulativo extends ActionBarActivity {
-
-    private Spinner spinner1;
+public class crear_acumulativo extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_activity_crear_acumulativo);
-
-
-        spinner1 = (Spinner) findViewById(R.id.sp_pacial);
-        String[] opciones = {"I", "II", "III", "IV"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
-        spinner1.setAdapter(adapter);
+        setContentView(R.layout.activity_crear_acumulativo);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_crear_acumulativo, menu);
+        getMenuInflater().inflate(R.menu.menu_crear_acumulativo, menu);
         return true;
     }
 
