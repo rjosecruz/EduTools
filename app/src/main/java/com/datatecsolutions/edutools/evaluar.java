67971c60bacea06949196fb0bf6b1a2e39a292c5
rjosecruz <rjosecruz@gmail.com>
2015-02-12 17:03:06 -0600

    Se agregaron los siguientes cambios: Se agregaron Iconos al action bar Los acumulativos ya se listan en cada clase Menu contextual para cada acumulativo

---
+++ app/src/main/java/com/datatecsolutions/edutools/evaluar.java
@@ -0,0 +1,46 @@
+package com.datatecsolutions.edutools;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+
+public class evaluar extends ActionBarActivity {
+    String codigo, nombre;
+    Bundle extras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evaluar);
+        extras = getIntent().getExtras();
+        codigo = extras.getString("Codigo");
+        nombre = extras.getString("Asignatura");
+        Toast.makeText(getApplicationContext(), codigo, Toast.LENGTH_LONG).show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_evaluar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
